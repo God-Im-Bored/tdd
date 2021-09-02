06c47a57a04e764a5c20d11f99fdd4611b1a3cf8
Aram <aram_martin@alumni.brown.edu>
2021-09-02 14:30:46 -0500

    FIX: css variable

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -1,31 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import './Calculator.css'
 
 class Calculator extends Component {
-    state = {
-        displayValue: '0',
-        numbers: [],
-        operators: [],
-        selectedOperator: '',
-        storedValue: ''
-    }
+  state = {
+    displayValue: "0",
+    numbers: [],
+    operators: [],
+    selectedOperator: "",
+    storedValue: "",
+  };
 
-    callOperator = () => {
-        console.log('call operation');
-      }
-    
-      setOperator = () => {
-        console.log('set operation');
-      }
-    
-      updateDisplay = () => {
-        console.log('update display');
-      }
-    
-      render = () => {
-        return (
-          <div className="calculator-container" />
-        );
-      }
+  callOperator = () => {
+    console.log("call operation");
+  };
+
+  setOperator = () => {
+    console.log("set operation");
+  };
+
+  updateDisplay = () => {
+    console.log("update display");
+  };
+
+  render = () => {
+    return <div className="calculator-container"></div>;
+  };
 }
 
-export default Calculator
+export default Calculator;
