31a6ab855e5a21c15cafec776ba28c7f86234240
Aram <aram_martin@alumni.brown.edu>
2021-09-02 13:35:29 -0500

    ADD: snapshot serializer

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Calculator = () => {
-    return (
-        <div id='calc-container'></div>
-    )
+class Calculator extends Component {
+    state = {
+        displayValue: '0',
+        numbers: [],
+        operators: [],
+        selectedOperator: '',
+        storedValue: ''
+    }
+
+    callOperator = () => {
+        console.log('call operation');
+      }
+    
+      setOperator = () => {
+        console.log('set operation');
+      }
+    
+      updateDisplay = () => {
+        console.log('update display');
+      }
+    
+      render = () => {
+        return (
+          <div className="calculator-container" />
+        );
+      }
 }
 
 export default Calculator
