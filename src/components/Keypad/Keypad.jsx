5625caf72a8f760779cd18d3ba310d76316aa460
Aram <aram_martin@alumni.brown.edu>
2021-09-03 11:03:56 -0500

    ADD: keypad props (nums & ops) to specs & jsx

--- src/components/Keypad/Keypad.jsx
+++ src/components/Keypad/Keypad.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Keypad = ({ callOperator, setOperator, updateDisplay, numbers, operators }) => (
+const Keypad = ({ callOperator, setOperator, updateDisplay, numbers, operators }) =>  {
+
+    const nums = numbers.map((number) => <p key={number}>{number}</p>)
+    const ops = operators.map((operator) => <p key={operator}>{operator}</p>)
+
+    return (
     <div className='keypad-container'>
+        <div className='numbers-container'>
+            {nums}
+        </div>
+        <div className='operators-container'>
+            {ops}
+        </div>
 
     </div>
 )
-
+}
 Keypad.propTypes = {
     callOperator: PropTypes.func.isRequired,
     setOperator: PropTypes.func.isRequired,
---
+++ src/components/Keypad/Keypad.spec.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import Keypad from './Keypad'
+
+describe('Keypad', () => {
+    let wrapper 
+
+    beforeEach(() => wrapper = shallow(  <Keypad
+        callOperator={jest.fn()}
+        setOperator={jest.fn()}
+        updateDisplay={jest.fn()} 
+        numbers={[]}
+        operators={[]}
+      />))
+
+      it('should render the values of numbers', () => {
+        wrapper.setProps({ numbers: ['0', '1', '2']})
+        expect(wrapper.find('.numbers-container').text()).toEqual('012')
+      })
+
+      it('should render the values of operators', () => {
+          wrapper.setProps({ operators: ['+', '-', '*', '/']})
+          expect(wrapper.find('.operators-container').text()).toEqual('+-*/')
+      })
+
+    it('should render 3 <div />s', () => {
+        expect(wrapper.find('div').length).toEqual(3)
+    })
+})
